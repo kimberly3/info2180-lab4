20936d4793de8f90417be4ef68afc1110e0d65f0
unknown <you@example.com>
2018-10-20 19:45:36 -0500

    second exercise

--- maze.js
+++ maze.js
@@ -1,15 +1,34 @@
 function setEvents(){
-//<!-- EXERCISE 1 -->
-	var hi =  document.getElementById("boundary1");
-	hi.onmouseover = function() {mouseOver()};
 
+
+	//<!-- EXERCISE 1 -->
+	var hi =  document.getElementById("boundary1");
+	hi.onmouseover = function() {
+		mouseOver()
+	};
+	
+	
+	//<!-- EXERCISE 2 -->
+
+	var hey= document.querySelectorAll("div .boundary");
+	for (let a=0; a < hey.length; a++){
+		hey[a].onmouseover= function(){
+	// loops through the hey array and turns all the boxes red
+	for(let i=0; i < hey.length; i++){
+		
+		var result= hey[i];
+		result.classList.add('youlose');
+	
+	}}}
+
+	
+	
 	function mouseOver() {
 		hi.classList.add('youlose');
 		
+		
+	}
 
-		//hi.style.backgroundColor = "red";
-		//hi.setAttribute('onMouseOver','red')
-}
 }
 
 
