5d475ff94e7c451067e8b9bebc6c9700c34d9742
unknown <you@example.com>
2018-10-21 14:57:04 -0500

    sixth and last exercise

--- maze.js
+++ maze.js
@@ -2,10 +2,12 @@ function setEvents(){
 
 
 	//<!-- EXERCISE 1 -->
+
 	var ex1 =  document.getElementById("boundary1");
 	ex1.onmouseover = function() {
 		mouseOver()
 	};
+
 
 	function mouseOver() {
 		ex1.classList.add('youlose');
@@ -16,37 +18,41 @@ function setEvents(){
 	//<!-- EXERCISE 2 -->
 	var ex5= document.getElementById("status");
 	var red= null;
-	var ex2= document.querySelectorAll("div .boundary");
-	for (let a=0; a < ex2.length; a++){
+	
+		var ex2= document.querySelectorAll("div .boundary");
+		for (let a=0; a < ex2.length; a++){
 		
-		ex2[a].onmouseover= function(){
+			ex2[a].onmouseover= function(){
 
-		// loops through the hey array and turns all the boxes red
-		for(let i=0; i < ex2.length; i++){
+		// loops through the ex2 array and turns all the boxes red
+			for(let i=0; i < ex2.length; i++){
 		
-			var result= ex2[i];
-			result.classList.add('youlose');
-			red= true;
-			//exercise 5
-			ex5.innerHTML="You LOSE! :("
+				var result= ex2[i];
+				result.classList.add('youlose');
+				red= true;
+				//exercise 5
+				ex5.innerHTML="You LOSE! :("
 			}
 		
 		}
 
 	}
+	
 
 	//<!-- EXERCISE 3 -->
-	
+
 	var ex3= document.getElementById("end");
 	ex3.onmouseover = function(){
 		if(red!= true){
 			alert("You WIN!!!");
 		}
 	}
-	
-	//<!-- EXERCISE 4 -->
 
+	
+	//<!-- EXERCISE 4 --> a START OVER
+	var start= null;
 	var ex4= document.getElementById("start");
+
 	ex4.onclick = function(){
 			//ex5.innerHTML="Move your mouse over the "S" to begin.";
 			for(let c=0; c < ex2.length; c++){
@@ -55,30 +61,70 @@ function setEvents(){
 					nresult.classList.remove('youlose');
 					red= false;
 
+				/*for(let j=0; j < ex6.length; j++){
+				var n2result= ex2[j];
 				
+					n2result.classList.remove('youlose');
+				}*/
 			}
 			ex5.innerHTML='Move your mouse over the "S" to begin.'
-		
+		start= true;
+		ex6.classList.remove('youlose');
 	};
 
 	//<!-- EXERCISE 5 -->
 
 	
-
+	var leave= null;
+	//if (leave ==true){
 	ex3.onmouseover = function(){
 		if(red!= true){
 			ex5.innerHTML="You WIN!!!";
+			leave=true;
 		}
 	};
+	
+	
+	//<!-- EXERCISE 6 -->
+	var ex6= document.getElementById("maze");
+	if (start!=false){
+		ex6.onmouseleave = function (){
+			
+			//ex6.classList.add('youlose');
+
+			//for (let a=0; a < ex2.length; a++){
+		
+			//ex2[a].onmouseover= function(){
+
+		// loops through the ex2 array and turns all the boxes red
+			for(let i=0; i < ex2.length; i++){
+		
+				var result= ex2[i];
+				result.classList.add('youlose');
+				red= true;
+				//exercise 5
+				ex5.innerHTML="You LOSE! :("
+			}
+		
+		};
+
+	}
+			
+		
+	
+
+	
+	/*ex6.onmouseleave = function (){
+		ex3.onmouseover = false;
+		leave=true;
+	}*/
 
 
 
+	
+	
+	
 }
-
-	
-	
-	
-
 
 
 
