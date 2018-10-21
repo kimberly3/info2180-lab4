228576bb98b1bc901ff0c33485bc6be6bb7753b0
unknown <you@example.com>
2018-10-20 20:28:18 -0500

    third exercise

--- maze.js
+++ maze.js
@@ -2,32 +2,47 @@ function setEvents(){
 
 
 	//<!-- EXERCISE 1 -->
-	var hi =  document.getElementById("boundary1");
-	hi.onmouseover = function() {
+	var ex1 =  document.getElementById("boundary1");
+	ex1.onmouseover = function() {
 		mouseOver()
 	};
-	
-	
-	//<!-- EXERCISE 2 -->
 
-	var hey= document.querySelectorAll("div .boundary");
-	for (let a=0; a < hey.length; a++){
-		hey[a].onmouseover= function(){
-	// loops through the hey array and turns all the boxes red
-	for(let i=0; i < hey.length; i++){
-		
-		var result= hey[i];
-		result.classList.add('youlose');
-	
-	}}}
-
-	
-	
 	function mouseOver() {
-		hi.classList.add('youlose');
+		ex1.classList.add('youlose');
 		
 		
 	}
+	
+	//<!-- EXERCISE 2 -->
+	var red= null;
+	var ex2= document.querySelectorAll("div .boundary");
+	for (let a=0; a < ex2.length; a++){
+		
+		ex2[a].onmouseover= function(){
+
+		// loops through the hey array and turns all the boxes red
+		for(let i=0; i < ex2.length; i++){
+		
+			var result= ex2[i];
+			result.classList.add('youlose');
+			red= true;
+		
+			}
+		
+		}
+
+	}
+	//<!-- EXERCISE 3 -->
+	
+	var ex3= document.getElementById("end");
+	ex3.onmouseover = function(){
+		if(red!= true){
+			alert("You WIN!!!");
+		}
+	}
+	
+	
+	
 
 }
 
